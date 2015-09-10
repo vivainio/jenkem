2d61905ba2b2b842eb26ebcd834b73e8ba1a2b39
Ville M. Vainio <ville.vainio@basware.com>
2015-09-10 21:41:59 +0300

    go command, helptexts, refactoring

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
 # Personalized jenkins experience
 
-Jenkins web ui can be slow. This app is faster, as it maintains job listing offline.
+Jenkins web ui can be slow to navigate, if you have lots of jobs. This app is faster, as it
+maintains job listing offline.
 
-Features:
+Uses [Peco](https://github.com/peco/peco) for searching jobs interactively in text console.
+Commands that take job name as argument launch peco if no job name is provided.
 
-- 'jenq sync' fetches job list from server
-- 'jenq jobs foobar-*-build' lists matching jobs
-- 'jenq fav JOBNAME' favorites JOBNAME
-- 'jenq favs' lists last 5 build statuses for all your favs
-- 'jenq log JOBNAME' outputs last build console output to console
+Installation:
 
-Todo:
+(soon to appear in PyPi, but in the meantime...)
 
-- Angular 2 UI (because I need an excuse to play with Angular 2)
-- Other stuff I use
+$ choco install peco
+$ git clone https://github.com/vivainio/jenkem.git
+$ cd jenkem
+$ pip install -r requirements.txt
+$ cd jenq
+$ python jenq.py set repository http://address-to-my-jenkins
+
+```
+$ python .\jenq.py -h
+usage: jenq.py [-h] {jobs,builds,log,set,sync,fav,favs,alias,jobinfo,go} ...
+
+positional arguments:
+  {jobs,builds,log,set,sync,fav,favs,alias,jobinfo,go}
+    jobs                List all jobs matching JOBPATTERN
+    builds              Show recent builds for JOB
+    log                 Show console output for JOB, build# BUILD (or latest)
+    set                 Set permanent config variable (e.g. 'jenq set
+                        repository http://foo-jenkins.busyware.com')
+    sync                Fetch list of jobs from repository to local cache
+    fav                 Favorite a job
+    favs                Show recent builds for all favorited jobs
+    alias               Create convenient alias for JOB
+    jobinfo             Display raw job info (json)
+    go                  Open newest build for JOB in browser
+
+optional arguments:
+  -h, --help            show this help message and exit
+```
+
+TODO:
+
+- Angular 2 Web UI (coming later)
+- Grepping console output for common error patterns
--- jenq/jenq.py
+++ jenq/jenq.py
@@ -10,6 +10,7 @@ import fnmatch
 import tempfile
 from subprocess import Popen, PIPE
 import os
+import webbrowser
 
 db = PickleShareDB("~/jenkem")
 
@@ -49,18 +50,29 @@ class Job:
 
     def builds(self):
         job_info = self.info()
-        #print "ji", job_info
+        pprint.pprint(job_info)
+        ##print "ji", job_info
         lastIdx = job_info['lastBuild']['number']
         firsIdx = max(lastIdx - 3, 1)
         for idx in range(lastIdx, firsIdx -1, -1):
-            build_info = j().get_build_info(self.name, idx,1)
-            print "  " + build_summary(build_info)
+            try:
+                build_info = j().get_build_info(self.name, idx,1)
+                #pprint.pprint(build_info)
+                print "  " + build_summary(build_info)
+            except jenkins.NotFoundException:
+                print "NOTFOUND #%d" % idx
 
     def log(self):
         job_info = self.info()
         lastIdx = job_info['lastBuild']['number']
         text = j().get_build_console_output(self.name, lastIdx)
         print text
+
+    @staticmethod
+    def find(jobname):
+        n = find_job_or_pick(jobname)
+        j = Job(n)
+        return j
 
 def jobs_c(args):
     jobs = find_jobs("*" + args.jobpattern+ "*")
@@ -125,7 +137,7 @@ def find_job_or_pick(jobname):
     aliased = db.get('alias', {}).get(jobname)
     if aliased:
         return aliased
-        
+
     jobs = db['jobs']
     if jobname is None:
         inp = "\n".join(j['name'] for j in jobs)
@@ -163,33 +175,58 @@ def alias_c(args):
     aliases[args.shortname] = jobname
     db['alias'] = aliases
 
+def find_job_info(jobname):
+    n = find_job_or_pick(args.jobname)
+    j = Job(n)
+    inf = j.info()
+    return inf, j
+
+
+def job_c(args):
+    j = Job.find(args.jobname)
+    j.builds()
+    pprint.pprint(j.info())
+
+def go_c(args):
+    j = Job.find(args.jobname)
+    inf = j.info()
+    pprint.pprint(inf)
+    url = inf['lastBuild']['url']
+    webbrowser.open(url)
+
 def main():
     args.init()
-    c = args.sub('jobs', jobs_c)
-    c.arg('jobpattern')
+    c = args.sub('jobs', jobs_c, help="List all jobs matching JOBPATTERN")
+    c.arg('jobpattern', metavar="JOBPATTERN")
 
-    c = args.sub('builds', builds_c)
-    c.arg('job')
+    c = args.sub('builds', builds_c, help="Show recent builds for JOB")
+    c.arg('job', metavar="JOB")
 
-    c = args.sub('log', log_c)
-    c.arg('jobname', nargs='?')
+    c = args.sub('log', log_c, help="Show console output for JOB, build# BUILD (or latest)")
+    c.arg('jobname', nargs='?', metavar="JOB")
 
-    c.arg('build', type=int, nargs='?')
+    c.arg('build', type=int, nargs='?', metavar="BUILD")
 
-    c = args.sub('set', set_c)
-    c.arg('variable')
-    c.arg('value')
+    c = args.sub('set', set_c, help="Set permanent config variable (e.g. 'jenq set repository http://foo-jenkins.busyware.com')")
+    c.arg('variable', metavar="VARIABLE")
+    c.arg('value', metavar="VALUE")
 
-    c = args.sub('sync', sync_c)
+    c = args.sub('sync', sync_c, help="Fetch list of jobs from repository to local cache")
 
-    c  = args.sub('fav', fav_c)
-    c.arg('jobname')
+    c  = args.sub('fav', fav_c, help="Favorite a job")
+    c.arg('jobname', nargs='?', metavar="JOB")
 
-    c = args.sub('favs', favs_c)
+    c = args.sub('favs', favs_c, help="Show recent builds for all favorited jobs")
 
-    c = args.sub('alias', alias_c)
-    c.arg('shortname', nargs='?')
-    c.arg('jobname', nargs='?')
+    c = args.sub('alias', alias_c, help="Create convenient alias for JOB")
+    c.arg('shortname', nargs='?', metavar="SHORTNAME")
+    c.arg('jobname', nargs='?', metavar="JOB")
+
+    c = args.sub('jobinfo', job_c, help="Display raw job info (json)")
+    c.arg('jobname', nargs='?', metavar="JOB")
+
+    c = args.sub('go', go_c, help="Open newest build for JOB in browser")
+    c.arg('jobname', nargs='?', metavar="JOB")
 
     args.parse()
 
